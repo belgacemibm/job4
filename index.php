c2202dc902c0724317d7c71723a643c6be3a5912
taxenco <carlos.beltran.exposito@gmail.com>
2020-04-09 12:07:30 +0100

    job listing

--- index.php
+++ index.php
@@ -3,7 +3,16 @@
 <?php
 $job = new Job;
 $template = new Template('template/frontpage.php');
-$template->jobs = $job->getAllJobs();
+$category = isset($_GET['category']) ? $_GET['category'] : null;
+
+if ($category) {
+    $template->jobs = $job->getByCategory($category);
+    $template->title = 'Jobs In ' . $job->getCategory($category)->name;
+} else {
+    $template->jobs = $job->getAllJobs();
+    $template->title = 'All Jobs';
+
+}
 $template->categories = $job->getCategories();
 echo $template;
 ?>
--- template/frontpage.php
+++ template/frontpage.php
@@ -2,16 +2,18 @@
 
       <div class="jumbotron">
         <h1>Find a job</h1>
-        <form>
-
+        <form method='GET' action='index.php'>
         <select name='category' class='form-control'>
           <option value = '0'>Choose category</option>
           <?php foreach ($categories as $category): ?>
            <option value = "<?php echo $category->id; ?>"><?php echo $category->name;?></option>
             <?php endforeach;?>
             </select>
+            <br>
+            <input type='submit' class='btn btn-lg btn-success' value = 'FIND'>
         </form>
       </div>
+      <h3><?php echo $title?></h3>
     <?php foreach ($jobs as $job): ?>
     <div class="row marketing">
         <div class="col-md-10">
